ced8045294bcb8ef66de0ca728a73fdbc7811b34
Mateuss18 <mateusdfl18@gmail.com>
2022-01-15 23:43:14 -0300

    Declarando as variaveis

---
+++ script.js
@@ -0,0 +1,14 @@
+const formulario = document.querySelector('form')
+
+const nome = document.querySelector('#nome')
+const idade = document.querySelector('#idade')
+const altura = document.querySelector('#peso')
+const imc = document.querySelector('#reseutadoImc')
+
+let dados = document.querySelectorAll('.pessoa')
+
+const btnEviar = document.querySelector('#btnEnviar')
+const btnLimpar = document.querySelector('#btnLimpar')
+
+btnEnviar.addEventListeners('click', function(e){
+})

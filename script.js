11157f1a4ac8e5b6b6392c4157becbc5f54f42a4
Mateuss18 <mateusdfl18@gmail.com>
2022-01-17 23:58:37 -0300

    Calculo imc concluido

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const formulario = document.querySelector('form')
+let formulario = document.querySelector('form')
 
-const inNome = document.querySelector('#nome')
-const inIdade = document.querySelector('#idade')
-const inAltura = document.querySelector('#altura')
-const inPeso = document.querySelector('#peso')
-const inImc = document.querySelector('#reseutadoImc')
+let inNome = document.querySelector('#nome')
+let inIdade = document.querySelector('#idade')
+let inAltura = document.querySelector('#altura')
+let inPeso = document.querySelector('#peso')
+let inImc = document.querySelector('#resultadoImc')
 
+let aviso = document.querySelector('#aviso')
 let dados = document.querySelectorAll('.pessoa')
 
-const btnEviar = document.querySelector('#btnEnviar')
-const btnLimpar = document.querySelector('#btnLimpar')
+let btnEnviar = document.querySelector('#btnEnviar')
+let btnLimpar = document.querySelector('#btnLimpar')
 
-btnEnviar.addEventListener('click', function(e){
+btnEnviar.addEventListener('click', function (e) {
     //Pegando os values dos inputs
     const nome = inNome.value
     const idade = inIdade.value
@@ -26,6 +27,24 @@ btnEnviar.addEventListener('click', function(e){
     console.log(imc)
 
     inImc.value = imc
-    const sit = situracaoDoPeso(imc)
+    let sit = situracaoDoPeso(imc)
     aviso.textContent = sit
+
+    let pessoa = {
+        nome : nome,
+        idade : idade,
+        altura : altura,
+        peso : peso,
+        imc : imc,
+        sit : sit
+    }
+
+    console.log(pessoa)
+    dados[1].textContent = pessoa.nome
+    dados[2].textContent = pessoa.idade + ' anos'
+    dados[3].textContent = pessoa.altura + ' Kg'
+    dados[4].textContent = pessoa.peso + ' m'
+    dados[5].textContent = pessoa.imc + ' ' + pessoa.sit
+
+    e.preventDefault()
 })

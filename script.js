00ad4408c8eddf18ac1af074009d395412022031
Mateuss18 <mateusdfl18@gmail.com>
2022-01-18 14:43:59 -0300

    Situacao do peso

--- script.js
+++ script.js
@@ -48,3 +48,24 @@ btnEnviar.addEventListener('click', function (e) {
 
     e.preventDefault()
 })
+
+// Função situação do peso
+function situracaoDoPeso(imc){
+    let situacao = ''
+    if(imc < 18.5){
+        situcao = 'Magreza'
+    } else if (imc >= 18.5 && imc <= 24.9){
+        situacao = 'Normal'
+    } else if (imc >= 25 && imc <= 29.9){
+        situacao = 'Sobrepeso'
+    } else if (imc >= 30 && imc <= 34.9){
+        situacao = 'Obesidade I'
+    } else if (imc >= 35 && imc <= 39.9){
+        situacao = 'Obesidade II'
+    } else if (imc >= 40){
+        situacao = 'Obesidade III'
+    }else{
+        situacao = 'Informe corretamente'
+    }
+    return situacao
+}

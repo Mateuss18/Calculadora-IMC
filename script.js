2a8673419061a8ef6e701aa2bc81876aab68975c
Mateuss18 <mateusdfl18@gmail.com>
2022-01-17 22:33:03 -0300

    Calculo imc

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const formulario = document.querySelector('form')
 
-const nome = document.querySelector('#nome')
-const idade = document.querySelector('#idade')
-const altura = document.querySelector('#peso')
-const imc = document.querySelector('#reseutadoImc')
+const inNome = document.querySelector('#nome')
+const inIdade = document.querySelector('#idade')
+const inAltura = document.querySelector('#altura')
+const inPeso = document.querySelector('#peso')
+const inImc = document.querySelector('#reseutadoImc')
 
 let dados = document.querySelectorAll('.pessoa')
 
 const btnEviar = document.querySelector('#btnEnviar')
 const btnLimpar = document.querySelector('#btnLimpar')
 
-btnEnviar.addEventListeners('click', function(e){
+btnEnviar.addEventListener('click', function(e){
+    //Pegando os values dos inputs
+    const nome = inNome.value
+    const idade = inIdade.value
+    const altura = inAltura.value
+    const peso = inPeso.value
+    const imc = (peso / (altura * altura)).toFixed(1)
+
+    console.log(nome)
+    console.log(idade)
+    console.log(altura)
+    console.log(peso)
+    console.log(imc)
+
+    inImc.value = imc
+    const sit = situracaoDoPeso(imc)
+    aviso.textContent = sit
 })

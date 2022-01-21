3e0929b4cbcc1e9cab2ef573c3e2f4cbdccce6b3
Mateuss18 <mateusdfl18@gmail.com>
2022-01-21 17:06:08 -0300

    Adicionando comentarios

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+//Pegando os elementos e transformando em variaveis
 var idade = document.getElementById("idade")
 var altura = document.getElementById("altura")
 var peso = document.getElementById("peso")
 var resultadoImc = document.getElementById("resultadoImc")
 
 document.getElementById("btnEnviar").addEventListener("click", validateForm);
+document.getElementById("btnLimpar").addEventListener("click", limparForm);
 
+//Validação do formulario
 function validateForm(){
     if(idade.value == '' || altura.value == '' || peso.value == ''){
         alert("Preencha todos os campos")
@@ -13,7 +16,7 @@ function validateForm(){
     }
 }
 
-
+//Função para fazer o calculo do IMC
 function calcularIMC(){
     var Pessoa = [idade.value, altura.value, peso.value]
 
@@ -21,19 +24,29 @@ function calcularIMC(){
 
     resultadoImc.value = imc.toFixed(1)
 
-    var resultado = ''
-    if(imc < 18.5){
-        resultado = 'Magreza'
-    }
-    else if(imc >= 18.5 && imc <= 24.9){
-        resultado = 'Normal'
-    }
-    else if(imc >= 25 && imc <= 29){
-        resultado = 'Sobrepeso'
-    }
-    else if(imc >= 30){
-        resultado = 'Obesidade'
+    function situacaoPeso(imc){
+        let situacao = ''
+        if(imc < 18.5){
+            situacao = 'Magreza'
+        }
+        else if(imc >= 18.5 && imc <= 24.9){
+            situacao = 'Normal'
+        }
+        else if(imc >= 25 && imc <= 29){
+            situacao = 'Sobrepeso'
+        }
+        else if(imc >= 30){
+            situacao = 'Obesidade'
+        }
+
+        return situacao
     }
 
-    aviso.textContent = resultado
+   let situacaoDoPeso = situacaoPeso(imc)
+    aviso.textContent = situacaoDoPeso
+}
+
+//Função para limpar a area de situacao do peso
+function limparForm(){
+    aviso.textContent = ''
 }
